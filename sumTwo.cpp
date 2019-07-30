1cfd57e8757d87b4a0b59782a8124a96f1d53dee
superchee <ggccqq0526LD@gmail.com>
2019-07-30 11:07:35 +0800

    two sum sort

--- sumTwo.cpp
+++ sumTwo.cpp
@@ -4,7 +4,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+int* twoSumBruteForce(int* nums, int numsSize, int target, int* returnSize){
     *returnSize = 0;
     for (int i = 0; i < numsSize; i++){
         for (int j = 0; j < numsSize; j++){
@@ -19,5 +19,49 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     }
     return NULL;
 
+}
+
+#define RETURN_SIZE 2
+
+int compare(const void* a, const void* b){
+    return (*(*(int**)a)-*(*(int**)b));
+}
+
+int* twoSumSort(int* nums, int numsSize, int target, int* returnSize){
+    int** numIdx = NULL;
+    int* returnIdx = NULL;
+    int nStart=0, nEnd=0, nSum=0;
+
+    numIdx = (int**)malloc(sizeof(int*)*numsSize);
+    for (int i=0; i<numsSize; i++){
+        numIdx[i] = &nums[i];
+    }
+
+    qsort(numIdx, numsSize, sizeof(int*), compare);
+
+    nStart = 0;
+    nEnd = numsSize -1;
+    while (nStart!=nEnd){
+        nSum = *numIdx[nStart] + *numIdx[nEnd] ;
+        if (nSum < target){
+            ++nStart;
+            continue;
+        }
+        else if(nSum == target){
+            *returnSize = 2;
+            returnIdx = (int*) malloc(sizeof(int)*RETURN_SIZE);
+            returnIdx[0] = numIdx[nStart] - nums;
+            returnIdx[1] = numIdx[nEnd] - nums;
+            return returnIdx;
+
+        }
+        else{
+            --nEnd;
+            continue;
+        }
+    }
+    *returnSize = 0;
+
+    return NULL;
 }
 
