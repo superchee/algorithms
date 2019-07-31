3193bd4a35449d05aa1294d98b17162ad4a22335
superchee <ggccqq0526LD@gmail.com>
2019-07-31 17:42:50 +0800

    ZigZagConversion failed

---
+++ ZigZagConversion.cpp
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <stdlib.h>
+char * convert(char * s, int numRows){
+    int nSize = 0, nIdx = 0, nNewIdx = 0;
+    char *returnChar = NULL;
+    nSize = strlen(s);
+    returnChar = (char *)malloc(sizeof(char)*(nSize +1));
+
+    for (int r = 0; r < numRows; r++){
+        nIdx = r;
+        if (r == 0){
+            while(nIdx < nSize){
+                returnChar[nNewIdx++] = s[nIdx];
+                nIdx += 2*numRows - 2;
+            }
+        }
+        else if(r == numRows - 1){
+            while(nIdx < nSize){
+                returnChar[nNewIdx++] = s[nIdx];
+                nIdx += 2*numRows - 2;
+            }
+        }
+        else{
+            while(nIdx < nSize){
+                returnChar[nNewIdx++] = s[nIdx];
+                nIdx += 2*(numRows -r) -2;
+                if (nIdx > nSize){
+                    break;
+                }
+                returnChar[nNewIdx++] = s[nIdx];
+                nIdx += 2*r -2;
+            }
+        }
+
+    }
+    returnChar[nSize] = '\0';
+    return returnChar;
+
+}
