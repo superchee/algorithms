27b4fb49cb3e403f0e1ecac8d570e08ea3a72ee8
superchee <ggccqq0526LD@gmail.com>
2019-08-01 09:53:29 +0800

    zigzag conversion

--- ZigZagConversion.cpp
+++ ZigZagConversion.cpp
@@ -1,6 +1,43 @@
 #include <string.h>
 #include <stdlib.h>
-char * convert(char * s, int numRows){
+
+
+char * convert(char *s, int numRows) {
+	int nSize = 0, nIdx = 0, nNewIdx = 0, nCircle = 0;
+	char *returnChar = NULL;
+
+	if (s == NULL)
+		return NULL;
+
+	if (numRows <= 1)
+		return s;
+
+	nCircle = 2 * numRows - 2;
+	nSize = strlen(s);
+	returnChar = (char*)malloc(sizeof(char)*(nSize + 1));
+	memset(returnChar, 0, sizeof(char)*(nSize + 1));
+
+	for (int i = 0; i < numRows; i++) {
+		nIdx = i;
+		while (nIdx < nSize)
+		{
+			returnChar[nNewIdx++] = s[nIdx];
+			nIdx += nCircle;
+			if (i != 0 && i != numRows - 1) {
+				nIdx -= 2 * i;
+				if (nIdx < nSize) {
+					returnChar[nNewIdx++] = s[nIdx];
+				}
+				nIdx += 2 * i;
+			}
+		}
+	}
+	return returnChar;
+	
+}
+
+
+char * convertSecond(char * s, int numRows){
     int nSize = 0, nIdx = 0, nNewIdx = 0;
     char *returnChar = NULL;
     nSize = strlen(s);
@@ -33,7 +70,7 @@ char * convert(char * s, int numRows){
             while(nIdx < nSize){
                 returnChar[nNewIdx++] = s[nIdx];
                 nIdx += 2*(numRows -r) -2;
-                if (nIdx > nSize){
+                if (nIdx >= nSize){
                     break;
                 }
                 returnChar[nNewIdx++] = s[nIdx];
