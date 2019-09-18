7896bd9ff4eb6ab9ea75aa91d0fbf5ff2c5cd984
superchee <ggccqq0526LD@gmail.com>
2019-09-18 14:58:13 +0800

    Sky line heap

--- Skyline.cpp
+++ Skyline.cpp
@@ -22,6 +22,10 @@ void swap(node* p, node* q){
 }
 
 int cmp(node* p, node* q){
+
+    if (p->h < q->h) return true;
+
+    /***
     if (p->x < q->x){
         return true;
     }else if(p->x == q->x){
@@ -29,11 +33,21 @@ int cmp(node* p, node* q){
             return true;
         }
     }
+    ***/
+
     return false;
 }
 
 int parent(int i){
     return i>>1;
+}
+
+int left(int i){
+    return i*2;
+}
+
+int right(int i){
+    return i*2+1;
 }
 
 void heapInit(heap* hq, int size){
@@ -42,7 +56,7 @@ void heapInit(heap* hq, int size){
 }
 
 void heapInsert(heap* hq, int x, int h){
-    int i = hq->size, nP = 0;
+    int i = ++hq->size, nP = 0;
 
     hq->head[i].x = x;
     hq->head[i].h = h;
@@ -56,11 +70,37 @@ void heapInsert(heap* hq, int x, int h){
             break;
         }
     }
-
-    hq->size++;
-    
 }
 
+node* heapTop(heap* hq){
+    return &hq->head[1];
+}
+
+node* heapPop(heap* hq){
+    if (hq->size == 0) return NULL;
+    swap(hq->head+1, hq->head+hq->size);
+    hq->size--;
+    heapify(hq, 1);
+    return &hq->head[hq->size];
+}
+
+void heapify(heap* hq, int i){
+    int l = left(i);
+    int r = right(i);
+    int minimum = 0;
+    if (l <= hq->size && cmp(hq->head+l, hq->head+i)){
+        minimum = l;
+    }else{
+        minimum = i;
+    }
+    if (r <= hq->size && cmp(hq->head+r, hq->head+minimum)){
+        minimum = r;
+    }
+    if (minimum != i){
+        swap(hq->head+i, hq->head+minimum);
+        heapify(hq, minimum);
+    }
+}
 
 /**
  * Return an array of arrays of size *returnSize.
@@ -71,17 +111,40 @@ int** getSkyline(int** buildings, int buildingsSize, int* buildingsColSize, int*
     *returnSize = 0;
     int** returnArray = (int**)malloc(sizeof(int*)*(2*buildingsSize));
     heap* hq = (heap*)malloc(sizeof(heap));
-    heapInit(hq, buildingsSize*2);
+    heapInit(hq, buildingsSize+1);
 
-    for (int i = 0; i < buildingsSize; i ++){
-        heapInsert(hq, buildings[i][0], -buildings[i][1]);
-        heapInsert(hq, buildings[i][0], buildings[i][2]);
+    int i = 0, temp = 0, height = 0;
+    node* top;
+
+    while(i < buildingsSize || hq->size!=0){
+        top = heapTop(hq);
+        if (hq->size == 0 || (i < buildingsSize && buildings[i][0] <= -top->x)){
+            temp = buildings[i][0];
+            while (i < buildingsSize && buildings[i][0] == temp){
+                heapInsert(hq, -buildings[i][1], -buildings[i][2]);
+                i++;
+            }
+        }else{
+            temp = -top->x;
+            while (hq->size > 0 && -heapTop(hq)->x <= temp){
+                //node* check = heapTop(hq);
+                heapPop(hq);
+            }
+        }
+        height = hq->size == 0? 0:-heapTop(hq)->h;
+        if (*returnSize == 0 || height != returnArray[(*returnSize)-1][1]){
+            returnArray[*returnSize] = (int*)malloc(sizeof(int)*2);
+            returnArray[*returnSize][0] = temp;
+            returnArray[*returnSize][1] = height;
+            (*returnSize)++;
+        }
     }
 
-    while (hq->size > 0){
-        
+    *returnColumnSizes = (int*)malloc(sizeof(int)*(*returnSize));
+    for(int j = 0; j < *returnSize; j++){
+        (*returnColumnSizes)[j] = 2;
     }
 
-    
 
+    return returnArray;
 }
